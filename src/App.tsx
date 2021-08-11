4e4f2e2a16708eeb28350fbd7b5b092168c0ef77
DoReVo <frz565@gmail.com>
2021-08-12 03:20:10 +0800

    chore: dev setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Tab } from "@headlessui/react";
 import React, { useState } from "react";
-
-import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl text-blue-600">SC2-Malaysia</h1>
+      <h1 className="text-center">SC2-Malaysia</h1>
+      <Tab.Group>
+        <Tab.List className="interval-group">
+          <Tab className="interval-item">Daily</Tab>
+          <Tab className="interval-item">Weekly</Tab>
+          <Tab className="interval-item">Monthly</Tab>
+        </Tab.List>
+      </Tab.Group>
     </div>
   );
 }
