3161140fc220b2de670f69996e05f7e8ffab4107
DoReVo <frz565@gmail.com>
2021-08-12 02:44:30 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-blue-600">SC2-Malaysia</h1>
+    </div>
+  );
+}
+
+export default App;
